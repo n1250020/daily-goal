97ea6eb2089b52764d5ecdabc090b4fdd8f37528
n1250020 <cfkt1250020@gn.iwasaki.ac.jp>
2026-01-29 11:46:05 +0900

    アプリ画面
    C:\laragon\www\dailygoal の中にこの4つを入れて
    laragonを起動して http://localhost/dailygoal/calendar.php にアクセス

---
+++ カレンダー画面（仮）/chara.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>キャラ</title>
+</head>
+<style>
+
+</style>
+<body>
+
+    <div>
+        <button onclick="location.href='AI.php'">AI</button>
+        <button onclick="location.href='calendar.php'">カレンダー</button>
+        <button onclick="location.href='schedule.php'">予定</button>
+        <button onclick="location.href='chara.php'">アバター</button>
+    </div>
+</body>
+キャラ
+</html>
